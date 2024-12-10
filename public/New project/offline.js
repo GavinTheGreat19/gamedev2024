a130353c8c87c08af4870981f50967ca166cc06d
GavinTheGreat19 <pizzarulez19@gmail.com>
2024-12-10 11:19:13 -0800

    Add files via upload

---
+++ public/New project/offline.js
@@ -0,0 +1,32 @@
+﻿{
+	"version": 1733858259,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/sprite-sheet1.png",
+		"images/9patch.png",
+		"images/solidtile.png",
+		"images/spritefont.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/sprite4-sheet0.png",
+		"images/sprite5-sheet0.png",
+		"images/sprite6-sheet0.png",
+		"images/sprite7-sheet0.png",
+		"media/flavortown.m4a",
+		"media/flavortown.ogg",
+		"media/calm.m4a",
+		"media/calm.ogg",
+		"media/eat.m4a",
+		"media/eat.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
